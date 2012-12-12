d781ffb3045a8304778c737b905d05e2b07846c7
lassie <lassie116@gmail.com>
2012-12-12 22:57:06 +0900

    create Paper by Element

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 (function(){
-     var paper = Raphael(50, 35, 200, 200);
+     var div = document.getElementById("base");
+     // var paper = Raphael(50, 35, 200, 200);
+     var paper = Raphael(div, 200, 200);
      var r = paper.rect(0,0,200,200);
-     r.attr({fill:"#99F",stroke:"#99F",opacity:0.25});
+     r.attr({fill:"#99F",stroke:"#F00",opacity:0.25});
      var c1 = paper.circle(50,50,100);
      c1.attr({fill:"#F99",stroke:"#F99",opacity:0.5});
      var c2 = paper.circle(125,125,50);
